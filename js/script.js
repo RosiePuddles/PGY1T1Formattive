af4ca193463286c3f12b49fc7c481145a6a3b6b9
Rosie <rosiegbartlett@gmail.com>
2021-11-05 12:45:24 +0000

    last current version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function mailingListButtonPress() {
     alert("We don't have a mailing list. Nothing is real. The world is a lie and nothing matters anymore");
 }
+
+function randomArticle() {
+    alert("Congratulations! You're the one millionth visitor too this site!");
+    open("https://arxiv.org/pdf/2101.0" + Math.round(Math.random() * 1295 + 1000) + ".pdf")
+}
+
+function fakeSignUp() {
+    alert("no.");
+}
