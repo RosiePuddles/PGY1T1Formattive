8a94f6a0682dc850d8d6cf197d5ea048029ba32a
Rosie <rosiegbartlett@gmail.com>
2021-12-03 02:42:40 +0000

    Navbar updates
    Updated the navbar, so it's loaded from JS, meaning only one thing to update

--- js/script.js
+++ js/script.js
@@ -50,3 +50,42 @@ function signUpSelect() {
 function myReset(node) {
     node.classList.remove("invalid_input");
 }
+
+/**
+ * Loads the navbar contents to the pages navbar
+ */
+function loadNavBar() {
+    document.getElementById('navbarSupportedContent').innerHTML = `
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="about_us.html">About Us</a>
+    </li>
+    <li class="nav-item dropdown">
+        <a aria-expanded="false" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
+           id="reportsDropdown" role="button">
+            Reports
+        </a>
+        <ul aria-labelledby="reportsDropdown" class="dropdown-menu">
+            <li><a class="dropdown-item" href="reports.html">All reports</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="co2_emissions.html">CO<sub>2</sub> Emissions</a></li>
+            <li><a class="dropdown-item" href="fertilisers.html">Fertilisers</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="report_about.html">About our reports</a></li>
+        </ul>
+    </li>
+</ul>
+`;
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.getElementById('navCollapseButton').classList.add("navbar-dark");
+    } else {
+        document.getElementById('navCollapseButton').classList.add("navbar-light");
+    }
+}
